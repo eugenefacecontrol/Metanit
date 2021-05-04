d30f081a96db49fddce1d39489ba093aca68c8c1
EugeneFaceControl <eugenefacecontrol@gmail.com>
2021-05-04 23:12:29 +0300

    Variables are done

--- BasicsOfProgramming/BasicsOfProgramming/Variables.cs
+++ BasicsOfProgramming/BasicsOfProgramming/Variables.cs
@@ -7,14 +7,23 @@ namespace BasicsOfProgramming
         public static void AboutVariables()
         {
             // Переменная представляет именнованную область памяти, в которой хранится значение определенного типа
-            string name = "Hello"; // Переменная имеет тип, имя и значение
+            string name = "Yauheni"; // Переменная имеет тип, имя и значение
 
             /*
              * имя может содержать любые цифры, буквы и символ подчеркивания, при этом первый символ в имени должен быть буквой или символом подчеркивания
              * в имени не должно быть знаков пунктуации и пробелов
              * имя не может быть ключевым словом языка C#
              */
-            Console.WriteLine(name);
+            Console.WriteLine($"Hello, {name}");
+            // Определение:
+            string Name;
+            // Инициализация:
+            Name = "Egor";
+            Console.WriteLine($"First person: {name}, Another pesron: {Name}");
+
+            // Материалы:
+            // Переменные и типы данных в C#
+            // https://youtu.be/GeUrbwSYFvM
         }
     }
 }
