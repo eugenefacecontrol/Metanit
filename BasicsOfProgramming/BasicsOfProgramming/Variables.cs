6162227e6b7d8d24d5b1b943bbbc6be35ad2d884
EugeneFaceControl <eugenefacecontrol@gmail.com>
2021-05-08 14:51:27 +0300

    Variables refactoring

--- BasicsOfProgramming/BasicsOfProgramming/Variables.cs
+++ BasicsOfProgramming/BasicsOfProgramming/Variables.cs
@@ -2,23 +2,31 @@
 
 namespace BasicsOfProgramming
 {
-    class Variables
+    public class Variables : MainClass
     {
-        public static void AboutVariables()
+        public void AboutVariables()
         {
-            // Переменная представляет именнованную область памяти, в которой хранится значение определенного типа
-            string name = "Yauheni"; // Переменная имеет тип, имя и значение
 
-            /*
-             * имя может содержать любые цифры, буквы и символ подчеркивания, при этом первый символ в имени должен быть буквой или символом подчеркивания
-             * в имени не должно быть знаков пунктуации и пробелов
-             * имя не может быть ключевым словом языка C#
-             */
+            Console.WriteLine("Переменная представляет именнованную область памяти, в которой хранится значение определенного типа");
+            Console.WriteLine("Переменная имеет тип, имя и значение");
+            string name = "Yauheni"; 
+
+            var explanation = @"
+имя может содержать любые цифры, буквы и символ подчеркивания, при этом первый символ в имени должен быть буквой или символом подчеркивания
+в имени не должно быть знаков пунктуации и пробелов
+имя не может быть ключевым словом языка C#
+";
+            Console.WriteLine(explanation);
+             
             Console.WriteLine($"Hello, {name}");
-            // Определение:
-            string Name;
-            // Инициализация:
-            Name = "Egor";
+
+            Console.WriteLine("Определение:");
+            Console.WriteLine("string Name");
+
+            Console.WriteLine("Инициализация:");
+            Console.WriteLine("Name = \"Egor\"");
+
+            string Name = "Egor";
             Console.WriteLine($"First person: {name}, Another pesron: {Name}");
 
             // Материалы:
